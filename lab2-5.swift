d215efe0ba3319e6411c92ba95bb33f380a80dd6
Ryan Hahn <rhahn@claytonps.org>
2017-12-13 10:45:34 -0500

    Create lab2-5.swift

---
+++ lab2-5.swift
@@ -0,0 +1,16 @@
+var paces: [String: Double] = ["Easy": 10.0, "Medium": 8.0, "Fast": 6.0]
+print(paces)
+
+paces["Sprint"] = 4.0
+print(paces)
+
+paces.updateValue(7.5, forKey: "Medium")
+paces.updateValue(5.8, forKey: "Fast")
+print(paces)
+
+paces.removeValue(forKey: "Sprint")
+print(paces)
+
+if let userPace = paces["Medium"] {
+    print("Okay! I'll keep you at a \(userPace) minute mile pace.")
+}
